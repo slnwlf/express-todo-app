4d242a68408a464aae552ea04f614b1d785b13e4
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-11-12 18:10:36 -0800

    confirmed all CRUD routes work via Postman

--- server.js
+++ server.js
@@ -26,48 +26,9 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-// Todos that we don't need anymore 
-
-// var todos = [{
-// 	task: 'Get milk',
-// 	description: 'Go to the store',
-// 	id: 1
-// }, {
-// 	task: 'Ride bike',
-// 	description: 'Road bike ride at lunch',
-// 	id: 2
-// }, {
-// 	task: 'Learn Javascript',
-// 	description: 'Read a book about it',
-// 	id: 3
-// }];
-
-
 ////////////////
 // GET ROUTES //
 ////////////////
-
-// app.get('/api/todos/', function(req, res) {
-// 	res.json(todos);
-// });
-
-// app.get('/api/todos', function(req, res) {
-// 	res.render('index.hbs', todos);
-// });
-
-// app.get('/api/todos/:id', function(req, res) {
-// 	var todoId = parseInt(req.params.id);
-
-// foundTodo is the created ID?  Not sure. 
-
-var foundTodo = todos.filter(function(todo) {
-	return todo.id == todoId;
-})[0];
-
-// send foundTodo to Json as data
-
-// 	res.json(foundTodo);
-// });
 
 // get all todos
 app.get('/api/todos', function(req, res) {
@@ -98,23 +59,6 @@ app.get('/api/todos/:id', function(req, res) {
 //// Post routes  ////
 //////////////////////
 
-// app.post('/api/todos', function(req, res) {
-// 	// create new todo with form data (`req.body`)
-// 	var newTodo = req.body;
-
-// 	// set sequential id (last id in `todos` array + 1)
-// 	if (todos.length > 0) {
-// 		newTodo.id = todos[todos.length - 1].id + 1;
-// 	} else {
-// 		newTodo.id = 1;
-// 	}
-
-// 	// add newTodo to `todos` array
-// 	todos.push(newTodo);
-
-// 	// send newTodo as JSON response
-// 	res.json(newTodo);
-// });
 
 // create new todo
 app.post('/api/todos', function(req, res) {
@@ -142,17 +86,6 @@ app.post('/api/todos', function(req, res) {
     });
   });
 
-// app.delete('/api/todos/:id', function(req, res) {
-// 	var todoId = parseInt(req.params.id);
-
-// 	var todoToDelete = todos.filter(function(todo) {
-// 		return todo.id == todoId;
-// 	})[0];
-
-// 	todos.splice(todos.indexOf(todoToDelete), 1);
-
-// 	res.json(todoToDelete);
-// });
 
 //////////////////////
 //// Put routes //////
@@ -177,28 +110,6 @@ app.put('/api/todos/:id', function(req, res) {
 		});
 	});
 });
-
-// app.put('/api/todos/:id', function(req, res) {
-// 	var todoId = parseInt(req.params.id);
-// 	var todoToUpdate = todos.filter(function(todo) {
-// 		return todo.id == todoId;
-// 	})[0];
-
-// 	todoToUpdate.task = req.body.task;
-// 	todoToUpdate.description = req.body.description;
-
-// 	res.json(todoToUpdate);
-// });
-
-// hbs.registerHelper('list', function(context, options) {
-// 	var ret = "<ul>";
-
-// 	for (var i = 0, j = context.length; i < j; i++) {
-// 		ret = ret + "<li>" + options.fn(context[i]) + "</li>";
-// 	}
-
-// 	return ret + "</ul>";
-// });
 
 var server = app.listen(process.env.PORT || 3000, function() {
 	console.log('Example app listening at http://localhost:3000/');
